03b056b43df9ea6f9406e7526b08c1713ca2f1ba
lihanwen <lihanwen@imdada.cn>
2019-07-18 00:59:23 +0800

    fix

--- code/flink-train-java/src/main/java/com/imooc/flink/java/course02/StreamingWCJavaAPP.java
+++ code/flink-train-java/src/main/java/com/imooc/flink/java/course02/StreamingWCJavaAPP.java
@@ -1,6 +1,7 @@
 package com.imooc.flink.java.course02;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -14,6 +15,9 @@ import org.apache.flink.util.Collector;
  * 使用Java API来开发flink的实时处理应用程序
  *
  * wordcount统计的数据来源为socket
+ *
+ * 代码参考来源为：https://ci.apache.org/projects/flink/flink-docs-release-1.7/dev/api_concepts.html
+ * Basic API Concepts
  */
 public class StreamingWCJavaAPP {
 
@@ -42,7 +46,11 @@ public class StreamingWCJavaAPP {
 
         //监控接口：nc -l 9999
 
-        //step3:transform
+
+//        //step3-1:transform
+//        /**
+//         * Anonymous classes
+//         */
 //        text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
 //            @Override
 //            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
@@ -60,22 +68,36 @@ public class StreamingWCJavaAPP {
 //                .timeWindow(Time.seconds(5))
 //                .sum(1)
 //                .print();
-        
-        //step3:transform（自定义Tuple）
-        text.flatMap(new FlatMapFunction<String, WC>() {
 
-            @Override
-            public void flatMap(String value, Collector<WC> out) throws Exception {
-                String[] tokens = value.toLowerCase().split(",");
 
-                for (String token : tokens){
-                    if (token.length()>0){
-                        out.collect(new WC(token.trim(),1));
-                    }
-                }
-            }
-        })
+//        //step3-2:transform
+//        text.flatMap(new FlatMapFunction<String, WC>() {
+//
+//            @Override
+//            public void flatMap(String value, Collector<WC> out) throws Exception {
+//                String[] tokens = value.toLowerCase().split(",");
+//
+//                for (String token : tokens){
+//                    if (token.length()>0){
+//                        out.collect(new WC(token.trim(),1));
+//                    }
+//                }
+//            }
+//        })
+
+
+        //step3-3:transform
+        /**
+         * Specifying Transformation Functions
+         */
+        text.flatMap(new MyMapFunction())
+                /**
+                 * Define keys using Field Expressions
+                 */
                 //.keyBy("word")
+                /**
+                 * Define keys using Key Selector Functions
+                 */
                 .keyBy(new KeySelector<WC, String>() {
                     @Override
                     public String getKey(WC value) throws Exception {
@@ -125,6 +147,26 @@ public class StreamingWCJavaAPP {
                     "word='" + word + '\'' +
                     ", count=" + count +
                     '}';
+        }
+    }
+
+    /**
+     * Implementing an interface
+     * The most basic way is to implement one of the provided interfaces
+     */
+    static class MyMapFunction implements FlatMapFunction<String, WC> {
+
+        @Override
+        public void flatMap(String value, Collector<WC> out) throws Exception {
+
+            String[] tokens = value.toLowerCase().split(",");
+
+            for (String token : tokens){
+                if (token.length()>0){
+                    out.collect(new WC(token.trim(),1));
+                }
+            }
+
         }
     }
 
