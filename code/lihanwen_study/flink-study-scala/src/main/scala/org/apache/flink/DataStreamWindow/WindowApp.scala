bb2a9f0f3c32e713f5e7cc41ed1ce1055f9d26b9
lihanwen <lihanwen@imdada.cn>
2019-09-26 00:39:27 +0800

    add window reduction app

--- code/lihanwen_study/flink-study-scala/src/main/scala/org/apache/flink/DataStreamWindow/WindowApp.scala
+++ code/lihanwen_study/flink-study-scala/src/main/scala/org/apache/flink/DataStreamWindow/WindowApp.scala
@@ -1,7 +1,7 @@
 package org.apache.flink.DataStreamWindow
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
 object WindowApp {
@@ -17,8 +17,9 @@ object WindowApp {
       .map((_,1))
       .keyBy(0)
       //.countWindow()
+      .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))//滚动窗口，采用flink处理时间
       //.timeWindow(Time.seconds(5))//滚动窗口
-      .timeWindow(Time.seconds(10),Time.seconds(5))//滑动窗口
+      //.timeWindow(Time.seconds(10),Time.seconds(5))//滑动窗口
       .sum(1)
       .print()
       .setParallelism(1)
---
+++ code/lihanwen_study/flink-study-scala/src/main/scala/org/apache/flink/DataStreamWindow/WindowReduceApp.scala
@@ -0,0 +1,32 @@
+package org.apache.flink.DataStreamWindow
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object WindowReduceApp {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    /**
+      * 此处为了实现ReduceFunction，故scoket传入元素需为数值型
+      */
+    val data = env.socketTextStream("localhost",9999)
+
+    data.flatMap(_.split(","))
+      .map(x => (1,x.toInt))//传入数值：1 2 3 4 5转化为（1，1）（1，2）（1，3）（1，4）（1，5）
+      .keyBy(0)//因为key都是1，所以所有的元素都分配到一个task执行
+      .timeWindowAll(Time.seconds(5))
+      .reduce{(v1,v2) => {
+        println(v1 + "..." + v1)
+
+        (v1._1 , v1._2 + v2._2)
+      }}
+      .print()
+
+
+    env.execute("WindowReduceApp")
+
+
+  }
+
+}
